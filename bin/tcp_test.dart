74b58c30b11c7ddd6ad89c9d57010fc028ac159f
cooler <52409646+notcooler@users.noreply.github.com>
2023-09-29 17:58:28 +0200

    sucukA

---
+++ bin/tcp_test.dart
@@ -0,0 +1,5 @@
+import 'package:tcp_test/tcp_test.dart' as tcp_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tcp_test.calculate()}!');
+}
---
+++ test/tcp_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:tcp_test/tcp_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
